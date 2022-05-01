7625b6a33e071382abc7bb6c9c3675b2dfc139b7
Emre Guneyler <eguneys@gmail.com>
2022-05-01 04:26:18 +0300

    github actions

--- linksoli.sh
+++ linksoli.sh
@@ -1,5 +1,31 @@
+git clone https://github.com/eguneys/cardstwo
+cd cardstwo
+yarn install
+yarn build
+yarn link
+
+cd ..
+
 git clone https://github.com/eguneys/soli2d
 cd soli2d
 yarn install
 yarn build
 yarn link
+
+cd ..
+
+git clone https://github.com/eguneys/sol-expressions
+cd sol-expressions
+yarn install
+yarn link soli2d
+yarn bootstrap
+lerna exec yarn link
+
+cd ..
+
+git clone https://github.com/eguneys/soli2d-js
+cd soli2d-js
+yarn install
+yarn build
+lerna exec yarn link
+cd ..
